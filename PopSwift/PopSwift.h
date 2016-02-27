7256c75057b44404d8a0c25b6a3e701d28209db7
James Taylor <jameslintaylor@gmail.com>
2016-02-27 15:36:21 -0500

    Initial Commit

---
+++ PopSwift/PopSwift.h
@@ -0,0 +1,19 @@
+//
+//  PopSwift.h
+//  PopSwift
+//
+//  Created by James Taylor on 2016-02-27.
+//  Copyright © 2016 James Taylor. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PopSwift.
+FOUNDATION_EXPORT double PopSwiftVersionNumber;
+
+//! Project version string for PopSwift.
+FOUNDATION_EXPORT const unsigned char PopSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PopSwift/PublicHeader.h>
+
+
